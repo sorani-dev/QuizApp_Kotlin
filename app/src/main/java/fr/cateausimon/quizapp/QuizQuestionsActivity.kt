b75e6096300e4c70a5104bb3459ff3d40d6f9143
sorani-dev <sorani.dev@outlook.com>
2022-03-24 22:41:02 +0100

    feat: add quiz questions activity and class

---
+++ app/src/main/java/fr/cateausimon/quizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,16 @@
+package fr.cateausimon.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+val questionList = Constants.getQuestions()
+
+        Log.i("Question Size", "${questionList.size}")
+    }
+}
