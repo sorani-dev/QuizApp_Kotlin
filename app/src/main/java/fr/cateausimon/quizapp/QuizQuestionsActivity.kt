cd0a24ba99112c89c7ca17c5898565f4f1ce7962
sorani-dev <sorani.dev@outlook.com>
2022-03-24 23:21:35 +0100

    feat: add question list to QuizQuestionsActivity

--- app/src/main/java/fr/cateausimon/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/fr/cateausimon/quizapp/QuizQuestionsActivity.kt
@@ -3,14 +3,29 @@ package fr.cateausimon.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-val questionList = Constants.getQuestions()
+        val questionList = Constants.getQuestions()
 
         Log.i("Question Size", "${questionList.size}")
+
+        val currentPosition = 1
+        val question: Question = questionList[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "{}/{}".format(currentPosition, progressBar.max)
+
+        tv_question.text = question.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+
     }
 }
